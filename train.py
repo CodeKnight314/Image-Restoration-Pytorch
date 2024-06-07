43c69758be45f61075e0bf160571676c81f7b02d
CodeKnight314 <rtang24@nmhschool.org>
2024-06-06 17:24:34 -0700

    Modified relevant files

---
+++ train.py
@@ -0,0 +1,55 @@
+import torch 
+import torch.nn as nn 
+import configs
+from tqdm import tqdm
+
+def train_step(model, criterion, data, optimizer): 
+    """
+    """
+    optimizer.zero_grad()
+    clean_img, degraded_img = data 
+    sr_img = model(degraded_img)
+    loss = criterion(clean_img, sr_img)
+
+    optimizer.step() 
+
+    return loss.item()
+
+def valid_step(model, criterion, data, criterion_psnr):
+    """
+    """
+    clean_img, degraded_img = data 
+    sr_img = model(degraded_img)
+    loss = criterion(clean_img, sr_img)
+    psnr = criterion_psnr(clean_img, sr_img)
+
+    return loss.item(), psnr.item()
+
+def train(model, criterion, criterion_psnr, train_dl, valid_dl, optimizer, scheduler, epochs): 
+    """
+    """
+    best_loss = float("inf")
+    model.to(configs.device)
+
+    for epoch in range(1, epochs+1):
+
+        model.train() 
+        total_train_loss = 0.0
+        for i, data in tqdm(enumerate(train_dl)):
+            tr_loss = train_step(model, criterion=criterion, data=data, optimizer=optimizer)
+            total_train_loss+=tr_loss
+
+        model.eval()   
+        total_valid_loss = 0.0 
+        total_valid_psnr = 0.0      
+        for i, data in tqdm(enumerate(valid_dl)):
+            valid_loss, valid_psnr = valid_step(model=model, criterion=criterion, data=data, criterion_psnr=criterion_psnr)
+            total_valid_loss+=valid_loss 
+            total_valid_psnr+=valid_psnr
+
+def main(): 
+    """
+    """
+
+if __name__ == "__main__": 
+    main()
