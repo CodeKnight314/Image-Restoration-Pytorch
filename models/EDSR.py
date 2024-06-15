4cb5ed4a373d57fd4394e213aa0b399724331276
CodeKnight314 <rtang24@nmhschool.org>
2024-06-14 21:17:49 -0700

    Added EDSR-Multiscale

--- models/EDSR.py
+++ models/EDSR.py
@@ -12,7 +12,22 @@ class ResBlock(nn.Module):
         self.residual_scaler = residual_scaler
 
     def forward(self, x): 
-        return x + self.residual_scaler * self.res_block(x)
+        out = self.res_block(x)
+        out = self.residual_scaler * out 
+        out = x + out
+        return out
+    
+class UpsampleBlock(nn.Module):
+    def __init__(self, channels, scale_factor): 
+        super().__init__() 
+
+        self.conv = nn.Conv2d(channels, channels * (scale_factor) ** 2, kernel_size=3, stride=1, padding=1)
+        self.up = nn.PixelShuffle(scale_factor)
+    
+    def forward(self, x): 
+        out = self.conv(x)
+        out = self.up(x)
+        return out
     
 class EDSR_SingleScale(nn.Module): 
     def __init__(self, input_channels, residual_channels, output_channels, num_resblocks, upscale_factor): 
@@ -25,14 +40,7 @@ class EDSR_SingleScale(nn.Module):
             layers.append(ResBlock(channels=residual_channels, residual_scaler=0.1))
         self.res_backbone = nn.Sequential(*layers)
 
-        if upscale_factor == 2: 
-            self.upsample = nn.Sequential(*[nn.Conv2d(residual_channels, residual_channels * 4, kernel_size=3, stride=1, padding=1),
-                                          nn.PixelShuffle(2)])
-        elif upscale_factor == 4: 
-            self.upsample = nn.Sequential(*[nn.Conv2d(residual_channels, residual_channels * 4, kernel_size=3, stride=1, padding=1),
-                                            nn.PixelShuffle(2),
-                                            nn.Conv2d(residual_channels, residual_channels * 4, kernel_size=3, stride=1, padding=1), 
-                                            nn.PixelShuffle(2)])
+        self.upsample = UpsampleBlock(residual_channels, upscale_factor)
             
         self.feature_reconstruction = nn.Conv2d(residual_channels, output_channels,kernel_size=3, stride=1, padding=1)
 
@@ -43,6 +51,37 @@ class EDSR_SingleScale(nn.Module):
         fc_output = self.feature_reconstruction(up_output)
 
         return x + fc_output
+
+class EDSR_MultiScale(nn.Module): 
+    def __init__(self, input_channels, residual_channels, output_channels, num_resblocks): 
+        super().__init__() 
+
+        self.feature_extraction = nn.Conv2d(input_channels, residual_channels, kernel_size=3, stride=1, padding=1)
+
+        layers = [] 
+        for i in range(num_resblocks): 
+            layers.append(ResBlock(residual_channels=residual_channels, residual_scaler=0.1))
+        self.res_backbone = nn.Sequential(*layers)
+
+        self.upsample_2x = UpsampleBlock(residual_channels, 2)
+        self.upsample_3x = UpsampleBlock(residual_channels, 3)
+        self.upsample_4x = UpsampleBlock(residual_channels, 4)
+
+        self.feature_reconstruction = nn.Conv2d(residual_channels, output_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x, scale_factor): 
+        fe_output = self.feature_extraction(x)
+        res_output = self.res_backbone(fe_output)
+
+        if scale_factor == 2: 
+            up_output = self.upsample_2x(res_output)
+        elif scale_factor == 3: 
+            up_output = self.upsample_3x(res_output)
+        else: 
+            up_output = self.upsample_4x(res_output)
+
+        out = self.feature_reconstruction(up_output)
+        return x + out
     
 def get_EDSR(input_channels = 3, residual_channels = 64, output_channels = 3, num_resblocks = 16, up_scale_factor = 2): 
     """
