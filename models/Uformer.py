c0cf9dd242bdef8ba4444e0e8e26457e64d6a2e8
CodeKnight314 <rtang24@nmhschool.org>
2024-06-19 18:00:06 -0700

    Modified W_MSA

--- models/Uformer.py
+++ models/Uformer.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import math
 
 class W_MSA(nn.Module):
     def __init__(self, channels, num_heads, window_size):
@@ -10,49 +8,49 @@ class W_MSA(nn.Module):
         self.window_size = window_size
         self.d_k = channels // num_heads
 
+        self.scale = (self.d_k) ** -0.5
+
         self.Q = nn.Conv2d(channels, self.d_k * self.num_heads, kernel_size=1, stride=1, padding=0)
         self.K = nn.Conv2d(channels, self.d_k * self.num_heads, kernel_size=1, stride=1, padding=0)
         self.V = nn.Conv2d(channels, self.d_k * self.num_heads, kernel_size=1, stride=1, padding=0)
         self.proj = nn.Conv2d(self.d_k * self.num_heads, channels, kernel_size=1, stride=1, padding=0)
-        self.attn_drop = nn.Dropout(0.1)
-        self.proj_drop = nn.Dropout(0.1)
 
-    def window_partition(self, x):
-        B, C, H, W = x.shape
-        x = x.view(B, C, H // self.window_size, self.window_size, W // self.window_size, self.window_size)
-        x = x.permute(0, 2, 4, 3, 5, 1).contiguous().view(-1, self.window_size ** 2, C)
-        return x
-
-    def window_reverse(self, x, H, W):
-        B = int(x.shape[0] / (H * W / self.window_size ** 2))
-        x = x.view(B, H // self.window_size, W // self.window_size, self.window_size, self.window_size, -1)
-        x = x.permute(0, 5, 1, 3, 2, 4).contiguous().view(B, -1, H, W)
-        return x
-
-    def forward(self, x):
+    def window_partition(self, x): 
         B, C, H, W = x.shape 
-        x = self.window_partition(x)
-
-        Q = self.Q(x).reshape(-1, self.num_heads, self.window_size ** 2, C // self.num_heads)
-        K = self.K(x).reshape(-1, self.num_heads, self.window_size ** 2, C // self.num_heads)
-        V = self.V(x).reshape(-1, self.num_heads, self.window_size ** 2, C // self.num_heads)
-
-        attn_output = self.attn_map(Q, K, V)
-
-        attn_output = attn_output.reshape(-1, self.window_size ** 2, C)
-        x = self.window_reverse(attn_output, H, W)
-
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        
+        x = x.reshape(B, H // self.window_size, self.window_size, W // self.window_size, self.window_size, C)
+        x = x.permute(0, 5, 1, 3, 2, 4).contiguous() 
+        x = x.view(B, C, -1, self.window_size * self.window_size)
         return x
 
-    def attn_map(self, Q, K, V):
-        attn_score = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
-        QK_prob = torch.softmax(attn_score, dim=-1)
-        QK_prob = self.attn_drop(QK_prob)
-        output = torch.matmul(QK_prob, V)
-        return output
+    def window_unpartition(self, x, H, W): 
+        B, _, _, C = x.shape
+        x = x.view(B, -1, self.window_size, self.window_size, C)
+        x = x.permute(0, 4, 1, 2, 3).contiguous()
+        x = x.reshape(B, C, H, W)
+        return x
+
+    def attn_map(self, Q, K, V): 
+        QK_probs = torch.matmul(Q, K.transpose(-2, -1)) * self.scale
+        attn_score = torch.softmax(QK_probs, dim=-1)
+        scaled_values = torch.matmul(attn_score, V)
+        return scaled_values
+    
+    def forward(self, x): 
+        B, C, H, W = x.shape
+        partitioned_inputs = self.window_partition(x)
+
+        Q = self.Q(partitioned_inputs).permute(0, 2, 3, 1).contiguous().reshape(B, self.num_heads, -1, self.window_size * self.window_size, self.d_k)
+        K = self.K(partitioned_inputs).permute(0, 2, 3, 1).contiguous().reshape(B, self.num_heads, -1, self.window_size * self.window_size, self.d_k)
+        V = self.V(partitioned_inputs).permute(0, 2, 3, 1).contiguous().reshape(B, self.num_heads, -1, self.window_size * self.window_size, self.d_k)
+
+        attn_map = self.attn_map(Q, K, V).reshape(B, -1, self.window_size * self.window_size, self.d_k * self.num_heads)
+
+        unpartioned_output = self.window_unpartition(attn_map, H, W)
+
+        projection_output = self.proj(unpartioned_output)
+
+        return projection_output
+
     
 class LeFF(nn.Module):
     def __init__(self, in_channels, hidden_dim, kernel_size=3):
@@ -79,4 +77,25 @@ class LeFF(nn.Module):
         x = self.dropout(x)
         x = x.view(B, H, W, C)
         
+        return x
+    
+class LeWinTransformerBlock(nn.Module):
+    def __init__(self, channels, num_heads, window_size, hidden_dim):
+        super(LeWinTransformerBlock, self).__init__()
+        self.norm1 = nn.LayerNorm(channels)
+        self.norm2 = nn.LayerNorm(channels)
+        self.w_msa = W_MSA(channels, num_heads, window_size)
+        self.leff = LeFF(channels, hidden_dim)
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.w_msa(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.leff(x)
+        x = x + shortcut
+
         return x
